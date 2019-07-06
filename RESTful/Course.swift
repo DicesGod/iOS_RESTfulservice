6b09faae2069109a7446c43577a7ceed5df76450
Minh Le <workonly140287@gmail.com>
2019-07-05 21:13:18 -0400

    add table view to display courses

---
+++ RESTful/Course.swift
@@ -0,0 +1,9 @@
+//
+//  Course.swift
+//  RESTful
+//
+//  Created by Minh Le on 2019-07-04.
+//  Copyright © 2019 Minh Le. All rights reserved.
+//
+
+import Foundation
---
+++ RESTful/CourseTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  CourseTableViewCell.swift
+//  RESTful
+//
+//  Created by Minh Le on 2019-07-04.
+//  Copyright © 2019 Minh Le. All rights reserved.
+//
+
+import UIKit
+
+class CourseTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
